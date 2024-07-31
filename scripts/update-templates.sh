24bebc2effbe4a71dffc7c3a04ea17279f2a720a
Davide Carpini <carpini.davide@gmail.com>
2024-07-31 17:31:36 +0200

    fix: update templates

---
+++ scripts/update-templates.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Define templates
+REPOS=(
+  "https://github.com/vechain/x-app-template.git x-app-template"
+  "https://github.com/vechain/react-dapp-template.git react-dapp-template"
+)
+
+# Define the template folder name
+TEMPLATES_DIR="templates"
+
+# Check if the template folder exists
+if [ -d "$TEMPLATES_DIR" ]; then
+    # Remove the template folder and its contents
+    echo "Removing existing template folder..."
+    rm -rf "$TEMPLATES_DIR"
+fi
+
+# Recreate the template folder
+echo "Creating new template folder..."
+mkdir "$TEMPLATES_DIR"
+
+# Cloning repositories
+for repo in "${REPOS[@]}"; do
+  url=$(echo "$repo" | cut -d ' ' -f 1)
+  folder=$(echo "$repo" | cut -d ' ' -f 2)
+  
+  echo "Cloning repository $url into $TEMPLATES_DIR/$folder..."
+  git clone "$url" "$TEMPLATES_DIR/$folder"
+  
+  echo "Removing .git folder from $TEMPLATES_DIR/$folder..."
+  rm -rf "$TEMPLATES_DIR/$folder/.git"
+done
+
+echo "Templates correctly cloned!"
